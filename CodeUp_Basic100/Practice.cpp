e8715f622dd1916780f1851ba4bf5cd2ed884a71
DdiDding <kdjwand@naver.com>
2022-06-09 23:37:30 +0900

    정수 3개 입력받아 합과 평균 출력하기

--- CodeUp_Basic100/Practice.cpp
+++ CodeUp_Basic100/Practice.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main()
 {
-	int64_t a;
-	scanf("%lld", &a);
-	printf("%lld", ++a);
+	int a,b,c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%lld\n%.1f", (long long)a + b + c, ((float)a+b+c)/3);
 	return 0;
 }
