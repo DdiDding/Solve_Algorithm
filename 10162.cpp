06d0325a0a7e07a98a987160511fcd77334db9c0
DdiDding <kdjwand@naver.com>
2022-07-26 05:12:02 +0900

    13305 주유소

--- 10162.cpp
+++ 10162.cpp
@@ -1,21 +1,26 @@
-#include<iostream>
-using namespace std;
-typedef unsigned long integer;
-int main()
-{
-	integer S = 0;
-	cin >> S;
-
-	integer total = 0;
-	integer cnt = 0;
-
-	while (total < S)
-	{
-		++cnt;
-		total += cnt;
-	}
-
-	if (0 < total - S) --cnt;
-	cout << cnt;
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int button[3] = { 300, 60, 10 };
+//	int buttonCnt[3] = { 0 };
+//
+//	int T = 0;
+//	cin >> T;
+//
+//	if (T % 10 != 0)
+//	{
+//		cout << -1;
+//		return 0;
+//	}
+//	
+//	for (int i = 0; i < 3; ++i)
+//	{
+//		buttonCnt[i] = T / button[i];
+//		T %= button[i];
+//	}
+//
+//	printf("%d %d %d", buttonCnt[0], buttonCnt[1], buttonCnt[2]);
+//	return 0;
+//}
--- 1789.cpp
+++ 1789.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
-using namespace std;
-typedef unsigned long integer;
-int main()
-{
-	integer S = 0;
-	cin >> S;
-
-	integer total = 0;
-	integer cnt = 0;
-
-	while (total < S)
-	{
-		++cnt;
-		total += cnt;
-	}
-
-	if (0 < total - S) --cnt;
-	cout << cnt;
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//typedef unsigned long integer;
+//int main()
+//{
+//	integer S = 0;
+//	cin >> S;
+//
+//	integer total = 0;
+//	integer cnt = 0;
+//
+//	while (total < S)
+//	{
+//		++cnt;
+//		total += cnt;
+//	}
+//
+//	if (0 < total - S) --cnt;
+//	cout << cnt;
+//	return 0;
+//}
