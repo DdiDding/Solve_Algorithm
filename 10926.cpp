256034270c57083bfb5ff86c1968dcf37ca483b8
DdiDding <kdjwand@naver.com>
2022-08-04 23:24:50 +0900

    10926 ??!

---
+++ 10926.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	char cha[51];
+	scanf("%s", &cha);
+	printf("%s??!", cha);
+
+	//or
+	/*string str;
+	cin >> str;
+	cout << str << "??!";*/
+	return 0;
+}
