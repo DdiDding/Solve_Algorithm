7c17303cc662c29a71e63ac74b2077f40b520d16
DdiDding <kdjwand@naver.com>
2022-08-10 02:21:18 +0900

    11654 아스키 코드

---
+++ 11654.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	char c;
+	cin >> c;
+	cout << (int)c;
+	return 0;
+}
