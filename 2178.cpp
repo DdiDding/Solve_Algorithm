33be431d37aa92390521bd5406aa5f94345dd7cd
DdiDding <kdjwand@naver.com>
2022-08-17 17:22:11 +0900

    2178 미로 탐색

--- 2178.cpp
+++ 2178.cpp
@@ -1,35 +1,66 @@
 #include <iostream>
 #include <stdio.h>
+#include <queue>
 #define MAX 101
-typedef unsigned short pos;
+#define X second
+#define Y first
+
 using namespace std;
 
-bool maze[MAX][MAX];
-struct Position
+int maze[MAX][MAX] = {0,};
+
+int pX[4] = { 0,1,0,-1 };
+int pY[4] = { 1,0,-1,0 };
+
+pair<int, int> s;
+pair<int, int> e;
+
+queue<pair<int, int>> q;
+void BFS( )
 {
-	Position() {}
-	Position(pos x, pos y)
-		:X(x),Y(y)
-	{}
-	unsigned short X;
-	unsigned short Y;
-};
+	q.push(make_pair(1,1));
+
+	while (q.empty() == false)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			int ny = q.front().Y + pY[i];
+			int nx = q.front().X + pX[i];
+
+			//도착하지 않았으면 인접노드 큐에 삽입 후 방문 처리
+			if (1 <= ny && ny <= e.Y && 1 <= nx && nx <= e.X)
+			{
+				if (maze[ny][nx] == 1)
+				{
+					q.push(make_pair(ny, nx));
+					maze[ny][nx] = maze[q.front().Y][q.front().X] + 1;
+				}
+			}
+			//도착했는지 확인
+			if (ny == e.Y && nx == e.X) return;
+		}
+		q.pop();
+	}
+}
 
 int main()
 {
-	Position end;
-	cin >> end.Y >> end.X;
-
-	for (int i = 1; i <= end.Y; ++i)
+	//1. Get input value
+	s = make_pair(1, 1);
+	cin >> e.Y>> e.X;
+	
+	for (int i = 1; i <= e.Y; ++i)
 	{
-		for (int j = 1; j <= end.X; ++j)
+		for (int j = 1; j <= e.X; ++j)
 		{
 			scanf("%1d", &maze[i][j]);
 		}
 	}
 
-	cout << DFS(Position(1,1),Position(1,1),end,0)<<endl;
-	cout << min;
+	//2. BFS
+	BFS();
 
+	//3. Print
+	cout << maze[e.Y][e.X];
 	return 0;
 }
