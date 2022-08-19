0a3d16eb6ba462f03ed73541f064ef829d73fece
DdiDding <kdjwand@naver.com>
2022-08-20 08:33:24 +0900

    1546 평

--- 1546.cpp
+++ 1546.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
-#include <functional>
-#include <vector>
-#include <queue>
-#include <map>
-#include <cstring>
-#include <stack>
+
 using namespace std;
+
+double grade[1001];
 
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
 
+	int N; cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> grade[i];
+	}
+	sort(grade, grade + N);
+
+	double result = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		result += (grade[i] / grade[N-1]) * 100;
+	}
+
+	cout << result / N;
 	return 0;
 }
