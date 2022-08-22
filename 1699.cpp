d87c6830b1cc2d59938390d73045245f295c9996
DdiDding <kdjwand@naver.com>
2022-08-22 10:29:42 +0900

    1699 제곱수

---
+++ 1699.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int dp[100'001];
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	int n; cin >> n;
+
+	dp[1] = 1;	dp[2] = 2;	dp[3] = 3;	dp[4] = 1;
+
+	for (int i = 5; i <= n; ++i)
+	{
+		dp[i] = i;
+		for (int j = 0; j *j <= i; ++j)
+		{
+			dp[i] = min(dp[i], dp[i - (j * j)] + 1);
+		}
+	}
+
+	cout << dp[n];
+	return 0;
+}
