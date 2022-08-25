88ccdc5d31f3910a71a1167c17add80af9d443cb
yeo myoung Kim <kdjwand@gmail.com>
2022-08-25 15:23:09 +0900

    10816 숫자 카드 2

--- 1920.cpp
+++ 1920.cpp
@@ -7,7 +7,7 @@ unordered_set<int> S;
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
-	
+
 	int N; cin >> N;
 	int temp;
 	for (int i = 0; i < N; ++i)
@@ -16,7 +16,7 @@ int main()
 		S.insert(temp);
 	}
 
-	int M,F; cin >> M;
+	int M, F; cin >> M;
 	for (int i = 0; i < M; ++i)
 	{
 		cin >> F;
@@ -25,3 +25,45 @@ int main()
 
 	return 0;
 }
+
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//int arr[100'002];
+//
+//int BinarySearch(const int * arr, const int & size, const int & f)
+//{
+//	int s = 0, e = size - 1, m;
+//
+//	while (s <= e)
+//	{
+//		m = (s + e) / 2;
+//
+//		if (arr[m] == f) return 1;
+//		if (arr[m] < f) s = m + 1;
+//		else e = m - 1;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0), cin.tie(0);
+//	
+//	int N; cin >> N;
+//	for (int i = 0; i < N; ++i)
+//	{
+//		cin >> arr[i];
+//	}
+//	sort(arr, arr + N);
+//
+//	int M,F; cin >> M;
+//	for (int i = 0; i < M; ++i)
+//	{
+//		cin >> F;
+//		cout << BinarySearch(arr, N, F) << "\n";
+//	}
+//
+//	return 0;
+//}
