0e521dd8fe65c00949641b8912440164fc58f2b4
yeo myoung Kim <kdjwand@gmail.com>
2022-08-28 16:19:36 +0900

    11051 이항 계수 2

---
+++ 11051.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#define MOD 10'007
+using namespace std;
+
+int dp[1001][1001];
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	int n, k; cin >> n >> k;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		dp[i][0] = 1;
+		dp[i][1] = i;
+	}
+
+	for (int i = 2; i <= n; ++i)
+	{
+		for (int j = 2; j <= i; ++j)
+		{
+			dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % MOD;
+		}
+	}
+
+	cout << dp[n][k];
+	return 0;
+}
