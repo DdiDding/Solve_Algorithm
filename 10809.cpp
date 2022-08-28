b8f944498fd7c05b36be325148277bcc6978191e
yeo myoung Kim <kdjwand@gmail.com>
2022-08-28 11:03:31 +0900

    1152 단어의 개수

--- 10809.cpp
+++ 10809.cpp
@@ -26,3 +26,19 @@ int main()
 	}
 	return 0;
 }
+
+#include <cstdio>
+int main() 
+{
+	int a[26] = { 0 }, i = 1, c; 
+	
+	for (; (c = getchar() - 97) != -87; i++)
+	{
+		a[c] ? 1 : a[c] = i;
+	}
+	
+	for (int b : a)
+	{
+		printf("%d ", --b);
+	}
+}
---
+++ 1152.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <cstring>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	string S;
+	int cnt = 0;
+	while (cin >> S)
+	{
+		++cnt;
+	}
+
+	cout << cnt;
+	return 0;
+}
