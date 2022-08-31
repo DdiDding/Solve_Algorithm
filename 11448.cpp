9f0f0be76ff19020737e324ce0fb807f5c6a0168
yeo myoung Kim <kdjwand@gmail.com>
2022-08-31 13:14:54 +0900

    11448 유레카 이론 수정

--- 11448.cpp
+++ 11448.cpp
@@ -1,55 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 50
 
-#define DP(x) if(dp[x] == 0) dp[x] = x * (x + 1) / 2
-
-
-int dp[1000];
+int dp[MAX];
+bool b[125000];
 
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
 	int t; cin >> t;
 
+	for (int i = 1; i < MAX; ++i)
+	{
+		dp[i] = i * (i + 1) / 2;
+	}
+	
+	for (int i = 1; i < MAX; ++i)
+	{
+		for (int j = 1; j < MAX; ++j)
+		{
+			for (int k = 1; k < MAX; ++k)
+			{
+				b[dp[i] + dp[j] + dp[k]] = true;
+			}
+		}
+	}
+
 	while (t--)
 	{
 		int n; cin >> n;
-
-		bool pass = false;
-		int result_1 = 0;
-		int result_2 = 0;
-		int result_3 = 0;
-
-		for (int i = 1; i < 1000; ++i)
-		{
-			DP(i);
-			result_1 = dp[i];
-			if (n < result_1) break;
-
-			for (int j = 1; j < 1000; ++j)
-			{
-				DP(j);
-				result_2 = dp[j];
-				if (n < result_1 + result_2) break;
-
-				for (int k = 1; k < 1000; ++k)
-				{
-					DP(k);
-					result_3 = dp[k];
-					if (n < result_1 + result_2 + result_3) break;
-
-					if (result_1 + result_2 + result_3 == n)
-					{
-						pass = true;
-						break;
-					}
-				}
-				if (pass == true) break;
-			}
-			if (pass == true) break;
-		}
-
-		cout << (pass ? "1" : "0") << '\n';
+		cout << b[n] << '\n';
 	}
 	return 0;
 }
@@ -86,5 +66,9 @@ Recursive(int i, int len)
 백트래킹 안될거 같음
 1000 * 1000 * 1000
 = 1'000'000'000
+
+100 * 100 ==10000
+
+50 * 50 = 2500
 
 */
