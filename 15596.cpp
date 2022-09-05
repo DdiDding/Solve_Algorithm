7d8eab1bf391a23eee791b6ec43bb1b9680d497c
yeo myoung Kim <kdjwand@gmail.com>
2022-09-05 23:29:00 +0900

    15596 정수 N개의 합 성공

---
+++ 15596.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long sum(std::vector<int> &a) {
+	long long ans = 0;
+	for (const auto & d : a)
+	{
+		ans += d;
+	}
+	return ans;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	return 0;
+}
