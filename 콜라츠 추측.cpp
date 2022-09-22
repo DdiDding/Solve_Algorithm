339950479f5f8c212c3f8dffc59f2afd6cb4c460
yeo myoung Kim <kdjwand@gmail.com>
2022-09-23 00:05:11 +0900

    콜라츠 추측 수정

--- 콜라츠 추측.cpp
+++ 콜라츠 추측.cpp
@@ -1,18 +1,14 @@
-include <bits / stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(int num) {
+int solution(int n) {
 	int answer = 0;
+	long long num = n;
 
 	while (++answer <= 500)
 	{
-		cout << num << endl;
-		if (num & 0x1 == 0x1)
-			num = num * 3 + 1;
-		else
-			num /= 2;
-
-		if (num == 1) return answer;
+		if (num == 1) return --answer;
+		(num & 0x1 == 0x1) ? num = num * 3 + 1 : num /= 2;
 	}
 	return -1;
 }
