25d4dfa5c81cb875a50518a5ecdd601daf1898ed
yeo myoung Kim <kdjwand@gmail.com>
2022-09-23 23:52:46 +0900

    서울에서 김서방 찾기 성공

--- 두  정수 사이의 합.cpp
+++ 두  정수 사이의 합.cpp
@@ -5,6 +5,10 @@ long long solution(int a, int b) {
 	long long n = abs(b - a) + 1;
 	long long answer = (a + b) * (n / 2);
 	if (n & 0x1 == 0x1) answer += (a + b) / 2;
-	cout << answer;
+
+	answer = (long long)b * -~b / 2 - (long long)a * ~- a / 2;
+
 	return answer;
 }
+
+
