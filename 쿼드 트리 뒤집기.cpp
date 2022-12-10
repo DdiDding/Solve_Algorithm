cf9cc32d238e9cd174810ace6e9f91b8ebc87940
yeo myoung Kim <kdjwand@naver.com>
2022-12-10 14:44:34 +0900

    쿼드 트리 뒤집기 개선

--- 쿼드 트리 뒤집기.cpp
+++ 쿼드 트리 뒤집기.cpp
@@ -3,26 +3,28 @@
 #include <string>
 using namespace std;
 
-queue<char> q;
+string str;
 
 //현재 레벨 트리의 각 4분면 string을 구하고 재 조합하여 반환한다!
 //불변식 : 이전레벨의 x는 없다.
-string Solve(int lev)
+string Solve(string::iterator& it, int lev)
 {
 	//base case : 1칸일때, 두 가지 경우가 있다.
 	//1)최소레벨일때의 1칸, 2)최소레벨이 되지 않았을 때의 한칸
-	if (lev == 0 || q.size() == 1)
+	if (lev == 0 || it + 1 == str.end())
 	{
-		string ret(1, q.front()); q.pop();
-		return ret;
+		return string(1, *(it++));
 	}
 
 	//1. 각 4분면의 문자열 저장
 	string temp[4];
 	for (int i = 0; i < 4; ++i)
 	{
-		temp[i] = q.front(); q.pop();
-		if (temp[i] == "x") temp[i] += Solve(lev - 1);
+		temp[i] = *(it++);
+		if (temp[i] == "x")
+		{
+			temp[i] += Solve(it, lev - 1);
+		}
 	}
 
 	//2. 재 합성후 반환
@@ -36,13 +38,12 @@ int main()
 	while (n--)
 	{
 		//1. Input value
-		queue<char> tq; q = tq;
-		string ts; cin >> ts;
-		for (auto & d : ts) q.push(d);
+		cin >> str;
 
 		//2. Do solve
-		string ret(1, q.front()); q.pop();
-		if (ret == "x") ret += Solve(20);
+		string::iterator it= str.begin();
+		string ret(1, *(it++));
+		if (ret[0] == 'x') ret += Solve(it, 20);
 		cout << ret << "\n";
 	}
 	return 0;
