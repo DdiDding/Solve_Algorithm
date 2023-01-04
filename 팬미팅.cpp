23b7640a17122edba676b784a74ba31b6918ca35
yeo myoung Kim <kdjwand@naver.com>
2023-01-05 00:18:28 +0900

    팬미팅 실패

--- 팬미팅.cpp
+++ 팬미팅.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <string>
 #include <string.h>
 using namespace std;
 
 int main()
 {
-
+	int t; cin >> t;
+	while (t--)
+	{
+		string h, f; cin >> h >> f;
+		
+	}
 
 	return 0;
 }   
+/*
+멤버와 팬들의 포옹 행사
+M명의 팬, 여성만 포옹
+이때 모든 멤버가 포옹하는 경우는 몇번인가
+
+     FFFFM
+FFFFFMMMMF
+
+*/
