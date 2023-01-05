7e001823f288db3e77919f78c2b1db1db2eec86e
yeo myoung Kim <kdjwand@naver.com>
2023-01-05 22:52:21 +0900

    9456 바텀업

--- 9465.cpp
+++ 9465.cpp
@@ -1,27 +1,9 @@
-#include <iostream>
-#include <algorithm>
-
-#define MAX 100'001
+#include <bits/stdc++.h>
 using namespace std;
+#define MAX 100'001
 
 int s[2][MAX];
 int dp[2][MAX];
-
-//s[y][x]를 골랐을 때의 최대 값을 반환
-//s : one based array
-//f(y, x) = min(s[y][x - 2], s[!y][x - 1]) + s[y][x]
-int solve(int y, int x)
-{
-	//기저사례 : 1열,2열 일때
-	if (x == 1) return s[y][1];
-	if (x == 2) return s[!y][1] + s[y][2];
-
-	//메모리제이션
-	int & ref = dp[y][x];
-	if (ref != -1) return ref; 
-
-	return ref = max(solve(!y,x - 1), solve(!y, x - 2)) + s[y][x];
-}
 
 int main()
 {
@@ -30,18 +12,30 @@ int main()
 	int T; cin >> T;
 	while (T--)
 	{
+		//1. Get input value
 		int n; cin >> n;
-		for (int i = 1; i <= n; ++i)
+		for (int y = 0; y <= 1; ++y)
 		{
-			cin >> s[0][i];
-		}
-		for (int i = 1; i <= n; ++i)
-		{
-			cin >> s[1][i];
+			for (int x = 1; x <= n; ++x)
+			{
+				cin >> s[y][x];
+			}
 		}
 
-		memset(dp, -1, sizeof(dp));
-		cout << max(solve(0, n), solve(1, n))<<'\n';
+		//2. Set initial value in DP table
+		memset(dp, 0, sizeof(dp));
+		dp[0][1] = s[0][1];
+		dp[1][1] = s[1][1];
+
+		//3. Fill in DP table
+		for (int i = 2; i <= n; ++i)
+		{
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + s[0][i];
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + s[1][i];
+		}
+
+		//4. Print
+		cout << max(dp[0][n], dp[1][n])<<'\n';
 	}
 
 	return 0;
@@ -105,4 +99,23 @@ int main()
 
 	return 0;
 }
+*/
+
+/*
+탑다운
+//s : one based array
+//s[y][x]를 골랐을 때의 최대 값을 반환
+int solve(int y, int x)
+{
+	//기저사례 : 1열,2열 일때
+	if (x == 1) return s[y][1];
+	if (x == 2) return s[!y][1] + s[y][2];
+
+	//메모리제이션
+	int & ref = dp[y][x];
+	if (ref != -1) return ref;
+
+	return ref = max(solve(!y,x - 1), solve(!y, x - 2)) + s[y][x];
+}
+
 */
