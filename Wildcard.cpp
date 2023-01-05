90e302f655e5ff6ac96e5cfd09f591d7ec1feecf
yeo myoung Kim <kdjwand@naver.com>
2023-01-06 07:39:44 +0900

    Wildcard DP

--- Wildcard.cpp
+++ Wildcard.cpp
@@ -1,6 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string w, s;
+int dp[101][101];
+
+//w가 s에 매칭이 되는지 bool형을 반환
+bool solve(int pw, int ps)
+{
+	int & ret = dp[pw][ps];
+ 	if (ret != -1) return ret;
+
+	while (pw < w.size() && ps < s.size() && (w[pw] == '?' || w[pw] == s[ps]))
+	{
+		++pw, ++ps;
+	}
+
+	if (pw == w.size()) return ret = (ps == s.size());
+
+	if (w[pw] == '*')
+	{
+		for (; ps <= s.size(); ++ps)
+		{
+			if (solve(pw + 1, ps) == 1) return ret = 1;
+		}
+	}
+
+	return ret = 0;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--)
+	{
+		cin >> w;
+		int n; cin >> n;
+
+		vector<string> ret;
+		for(int i = 0; i < n; ++i)
+		{
+			cin >> s;
+			memset(dp, -1, sizeof(dp));
+			if (solve(0, 0) == true) ret.push_back(s);
+		}
+
+		sort(ret.begin(), ret.end(), less<string>());
+		for (auto & d : ret)
+		{
+			cout << d << '\n';
+		}
+	}
+	return 0;
+}
+
+/*
+파일 이름의 일부만으로 파일 이름을 지정하는 방법
+?혹은 *특수 문자 포함
+
+?는 어떤 글자와 비교해도 일치한다.
+*는 0 글자 이상의 어떤 문자열에도 일치한다. (없어도 된다는뜻)
+
+와일드카드 문자열 w
+파일명의 개수 n
+
+f[pw][ps] = 
+
+*/
+
+//완전 탐색 방법
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
 //w가 s에 매칭이 되는지 bool형을 반환
 bool solve(string w, string s)
 {
@@ -30,7 +104,7 @@ int main()
 
 	int t; cin >> t;
 	vector<string> s;
-	
+
 	while (t--)
 	{
 		string w; cin >> w;
@@ -41,7 +115,6 @@ int main()
 			cin >> s[i];
 		}
 
-
 		vector<string> ret;
 		for (auto & d : s)
 		{
@@ -49,24 +122,11 @@ int main()
 		}
 
 		sort(ret.begin(), ret.end(), less<string>());
-		cout << "정답\n";
 		for (auto & d : ret)
 		{
 			cout << d << '\n';
 		}
-		cout << "끝'\n";
 	}
 	return 0;
 }
-
-/*
-파일 이름의 일부만으로 파일 이름을 지정하는 방법
-?혹은 *특수 문자 포함
-
-?는 어떤 글자와 비교해도 일치한다.
-*는 0 글자 이상의 어떤 문자열에도 일치한다. (없어도 된다는뜻)
-
-와일드카드 문자열 w
-파일명의 개수 n
-
 */
