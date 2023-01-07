9f7eb58e1a440630ebe46cd09b8e126e568ffd57
yeo myoung Kim <kdjwand@naver.com>
2023-01-08 02:30:48 +0900

    2193 완전탐색 성공

--- 1629.cpp
+++ 1629.cpp
@@ -1,63 +1,10 @@
+/* 제곱문제다 병신아 */
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int lli;
-lli a,b,c;
-lli solve(int b)
-{
-	if (b == 1) return a % c;
-
-	lli half = solve(b / 2) % c;
-	//홀수면
-	if (b & 0x1) return (half * half) % c * a % c;
-	//짝수면
-	return half * half % c;
-}
 
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
-	cin >> a >> b >> c;
-	cout << solve(b);
+
 	return 0;
 }
-
-/*
-A를 B번 곱한 수 알고 싶다.
-C로 나누기
-
-10 ^ 4 = 10'000
-10 ^ 2 ^ 2 = 100
-100 * 100 = 100'00
-(10 * 2) * 2
-
-7 * 9 = 63
-7 * 4 * 4 + 7
-
-
-*/
-
-/*
-#include <iostream>
-typedef long long  int ll;
-using namespace std;
-
-ll MOD(const ll & a, const ll & b, const ll & c)
-{
-	if (b == 1) return a % c;
-
-	ll half = MOD(a, b / 2, c);
-	half = half * half % c;
-	//b가 홀수
-	if (b & 0x1) half = half * a % c;
-	return half;
-}
-
-int main()
-{
-	ios::sync_with_stdio(0), cin.tie(0);
-	ll A, B, C; cin >> A >> B >> C;
-
-	cout << MOD(A,B,C);
-	return 0;
-}
-*/
--- 2193.cpp
+++ 2193.cpp
@@ -2,6 +2,66 @@
 #define MAX 91
 using namespace std;
 
+int dp[91];
+
+//점화식 : f(n) = 
+int solve(int n)
+{
+	//기저 사례
+	if (n == 1) return 1;
+	if (n == 2) return 1;
+	if (n == 3) return 2;
+
+	//풀이
+	int ret = 0;
+	for (int i = 1; i < n - 1; ++i)
+	{
+		ret += solve(i);
+	}
+	return ret + 1;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	int n; cin >> n;
+	cout << solve(n);
+	return 0;
+}
+
+/*
+0과 1로만 이루어진 수를 이진수라한다.
+이중 특별한 성질을 가진 수를 이친수
+0으로 시작하지 않는다.
+1이 두번 연속으로 나타나지 않는다.
+N자리의 이친수 개수를 구한다 
+
+f(n) = f(0) + f(1) + ... f(n -2)
+f(n) = f(n - 1) + f(n - 2)
+
+점화식
+f(1) = 1 [ 1 ]
+f(2) = 1 [ 10 ]
+f(3) = 2 [ 100, 101 ]
+f(4) = 3 [ 1000, 1001, 1010 ]
+f(5) = 5 [ 10000, 10001, 10010, 10100, 10101]
+f(6) = 8
+f(7) = 13
+f(8) = 21
+f(9) = 34
+f(10) = 55
+
+f(n) =  f(n - 1)은 못씀,
+
+
+*/
+
+//DP
+/*
+#include <iostream>
+#define MAX 91
+using namespace std;
+
 long long int dp[MAX][2];
 unsigned long long int dp2[MAX][2];
 
@@ -22,3 +82,4 @@ int main()
 	cout << dp[N][0] + dp[N][1];
 	return 0;
 }
+*/
