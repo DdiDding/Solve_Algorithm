7ef71e2162ddd00c2004e58681325a2711a38b8d
yeo myoung Kim <kdjwand@naver.com>
2023-01-09 15:26:45 +0900

    최대 증가 부분 수열 DP 실패

--- 최대 증가 부분 수열.cpp
+++ 최대 증가 부분 수열.cpp
@@ -1,6 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int s[505];
+int dp[505];
+
+//s[n]으로 끝나는 최대증가부분 수열의 길이
+int solve(int n)
+{
+	if (n == 0) return dp[n] = 1;
+
+	if (dp[n] != -1) return dp[n];
+
+	int ret = 1;
+	for (int i = 0; i < n; ++i)
+	{
+		if (s[i] < s[n]) ret = max(ret, (dp[i] = solve(i)) + 1);
+	}
+	return dp[n] = ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--)
+	{
+		//1. Get input value & Initialize dp table
+		int n; cin >> n;
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> s[i];
+		}
+		memset(dp, -1, sizeof(dp));
+
+		int ret = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			ret = max(ret, solve(i));
+		}
+
+		cout << ret << '\n';
+	}
+	return 0;
+}
+
+/*
+부분 수열 (순서 변경 x)
+순 증가 = 증가 부분 수열
+그 중 가장 긴 길이를 계산해라.
+
+점화식
+52436785
+11223453
+
+
+*/
+
+//종만북 재귀
+//시간 복잡도 = O(n^2)
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
 //s의 최대 증가 부분 수열을 반환한다.
 int solve(const vector<int>& s)
 {
@@ -37,22 +100,46 @@ int main()
 		{
 			cin >> s[i];
 		}
-		
+
 		cout << solve(s) << '\n';
 	}
 	return 0;
 }
-
-/*
-부분 수열 (순서 변경 x)
-순 증가 = 증가 부분 수열
-그 중 가장 긴 길이를 계산해라.
-
-점화식
-52436785
-11223453
-
-
 */
 
-//재귀
+//내 스타일의 재귀
+/*
+//s[n]으로 끝나는 최대증가부분 수열의 길이
+int solve(int n)
+{
+	if (n == 0) return 1;
+
+	int ret = 1;
+	//s[0] ~ s[n - 1] < s[n] 인
+	for (int i = 0; i < n; ++i)
+	{
+		if (s[i] < s[n]) ret = max(ret, solve(i) + 1);
+	}
+	return ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--)
+	{
+		//1. Get input value & Initialize dp table
+		int n; cin >> n;
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> s[i];
+		}
+
+		cout << solve(n - 1) << '\n';
+	}
+	return 0;
+}
+*/
