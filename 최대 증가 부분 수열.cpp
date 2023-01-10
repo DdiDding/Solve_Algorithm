a4e88f59f5a7b6643a69caf241379d85ee4b02fa
yeo myoung Kim <kdjwand@naver.com>
2023-01-10 15:20:07 +0900

    합친 LIS 실패

--- 최대 증가 부분 수열.cpp
+++ 최대 증가 부분 수열.cpp
@@ -7,10 +7,14 @@ int dp[505];
 //s[n]으로 끝나는 최대증가부분 수열의 길이
 int solve(int n)
 {
+	//기저사례
 	if (n == 0) return dp[n] = 1;
 
+	//메모리제이션
 	if (dp[n] != -1) return dp[n];
 
+	//이 이전원소중 자신보다 작은 원소의 LIS를 확인하며 값을 업데이트한다.
+	//최소 값(길이)은 1이다.
 	int ret = 1;
 	for (int i = 0; i < n; ++i)
 	{
@@ -59,7 +63,7 @@ int main()
 */
 
 //종만북 재귀
-//시간 복잡도 = O(n^2)
+//시간 복잡도 = O(2^n)
 /*
 #include <bits/stdc++.h>
 using namespace std;
@@ -139,6 +143,58 @@ int main()
 		}
 
 		cout << solve(n - 1) << '\n';
+	}
+	return 0;
+}
+*/
+
+//내 스타일의 DP
+//시간 복잡도 : O(n^2)
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int s[505];
+int dp[505];
+
+//s[n]으로 끝나는 최대증가부분 수열의 길이
+int solve(int n)
+{
+	if (n == 0) return dp[n] = 1;
+
+	if (dp[n] != -1) return dp[n];
+
+	int ret = 1;
+	for (int i = 0; i < n; ++i)
+	{
+		if (s[i] < s[n]) ret = max(ret, (dp[i] = solve(i)) + 1);
+	}
+	return dp[n] = ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--)
+	{
+		//1. Get input value & Initialize dp table
+		int n; cin >> n;
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> s[i];
+		}
+		memset(dp, -1, sizeof(dp));
+
+		int ret = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			ret = max(ret, solve(i));
+		}
+
+		cout << ret << '\n';
 	}
 	return 0;
 }
---
+++ 합친 LIS.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//JLIS의 길이를 출력한다.
+int solve(vector<int> a, vector<int> b)
+{
+
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	return 0;
+}
+
+/*
+두개의 증가 부분 수열을 합치고 오름차순 정렬한게 합친 증가 부분 수열,
+그중 가장 긴 수열을 합친 LIS.
+중복 원소는 제거 된다.
+
+1) 수열 A,B 에서 LIS를 찾는다.
+
+LIS를 합친다고 가장 길지는 않다!
+모든 증가 부분 수열을 더해봐야함!
+
+이건 일단 포기함
+*/
