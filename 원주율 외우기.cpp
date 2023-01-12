efbbb75dba55d82b9d2137451b90b13675f8452e
yeo myoung Kim <kdjwand@naver.com>
2023-01-12 11:00:41 +0900

    원주율 외우기 완전 탐색 성공

--- 원주율 외우기.cpp
+++ 원주율 외우기.cpp
@@ -1,18 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
+static const int MAX = 987654321;
 
-//수열 S의 최소 난이도
-void solve(vector<int> s)
+int GetHard(const string& s)
 {
-	//기저사례
-	if()
+	//난이도 1 검사
+	char std = s[0];
+	bool isPass = true;
+	for (int i = 1; i < s.size() && isPass; ++i)
+	{
+		if (std != s[i]) isPass = false;
+	}
+	if (isPass == true) return 1;
 
+	//난이도 2 검사
+	isPass = true;
+	int sign = s[0] < s[1] ? 1 : -1;
+	for (int i = 0; i < s.size() - 1 && isPass; ++i)
+	{
+		if (s[i] + sign != s[i + 1]) isPass = false;
+	}
+	if (isPass == true) return 2;
+
+	//난이도 4 검사
+	isPass = true;
+	bool bSwitch = false;
+	char stdarr[2]; stdarr[0] = s[0]; stdarr[1] = s[1];
+	for (int i = 2; i < s.size() && isPass; ++i)
+	{
+		if (stdarr[bSwitch] != s[i]) isPass = false;
+		bSwitch = !bSwitch;
+	}
+	if (isPass == true) return 4;
+
+	//난이도 5 검사
+	isPass = true;
+	int diff = s[1] - s[0];
+	for (int i = 1; i < s.size() - 1 && isPass; ++i)
+	{
+		if (s[i] + diff != s[i + 1]) isPass = false;
+	}
+	if (isPass == true) return 5;
+
+	//나머지 난이도는 10
+	return 10;
+}
+
+//수열 S의 최소 난이도를 반환한다.
+int solve(string s)
+{
+	//기저사례 : 조각의 자리가 2이하일 때, 5이하일 때
+	if (s.size() <= 2)  return MAX;
+	if (s.size() <= 5)  return GetHard(s);
+
+	//아니라면 3, 4, 5씩 쪼갠다.
+	int front, back, ret = MAX;
+	for (int i = 3; i <= 5; ++i)
+	{
+		if (3 <= s.size() - i)
+		{
+			front = solve(s.substr(0, i));
+			back = solve(s.substr(i));
+			//cout << s.substr(0, i) << "(" << front << ")와 " << s.substr(i) << "(" << back << ")을 합쳐 결과 " << front + back << "이 나왔습니다\n";
+			ret = min(ret, front + back);
+		}
+	}
+	
+	return ret;
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
+
+	int t; cin >> t;
+	string s; s.reserve(10'005);
+	while(t--)
+	{
+		string str; cin >> str;
+		cout << "정답" << solve(str) << '\n';
+	}
 
 	return 0;
 }
@@ -22,5 +90,6 @@ int main()
 부분 수열의 난이도 합을 구해라
 
 https://algospot.com/judge/problem/read/PI
+
 
 */
--- 합친 LIS.cpp
+++ 합친 LIS.cpp
@@ -1,10 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
+int n, m;
+int A[101], B[101];
 
 //JLIS의 길이를 출력한다.
-int solve(vector<int> a, vector<int> b)
+int solve(int a, int b)
 {
+	int ret = 0;
+	ll aa = (a == -1 ? -1 : A[a]);
+	ll bb = (b == -1 ? -1 : B[b]);
+	ll maxValue = max(aa, bb);
 
+	for (int nextA = a + 1; nextA < n; nextA++)
+	{
+		if (maxValue < A[nextA])
+		{
+			ret = max(ret, solve(nextA, b) + 1);
+		}
+	}
+	for (int nextB = b + 1; nextB < m; nextB++)
+	{
+		if (maxValue < B[nextB])
+		{
+			ret = max(ret, solve(a, nextB) + 1);
+		}
+	}
+	return ret;
 
 }
 
@@ -13,25 +35,37 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 
+	int c; cin >> c;
+	for (int k = 0; k < c; k++)
+	{
+		cin >> n >> m;
+		for (int i = 0; i < n; i++) cin >> A[i];
+		for (int i = 0; i < m; i++) cin >> B[i];
+
+		solve(0, 0);
+	}
+
 	return 0;
 }
 
 /*
-두개의 증가 부분 수열을 합치고 오름차순 정렬한게 합친 증가 부분 수열,
-그중 가장 긴 수열을 합친 LIS.
-중복 원소는 제거 된다.
+두개의 증가 부분 수열을 합치고 중복없이 오름차순 정렬한게 합친 증가 부분 수열,
+=합칠때는 자동으로 정렬되고 중복값을 못받는 컨테이너 Set 이용
 
-1) 수열 A,B 에서 LIS를 찾는다.
 
 LIS를 합친다고 가장 길지는 않다!
-모든 증가 부분 수열을 더해봐야함!
+모든 증가 부분 수열을 더해봐야함! - 진짜?
+
+탐욕적으로 한번 생각해볼수 있지 않을까?
+
+각 원소로 끝나는 최대 증가 부분 수열만 선택해도 괜찮을거 같은데?
 
 "슈도 코드"
-1. for(A의 f(1 ~n)으로 끝나는 증가 부분 수열
+1. for(A[0] ~ A[n]으로 끝나는 최대 증가 부분 수열 aa)
 {
-	1-1.for(B의 f(1 ~ n)으로 끝나는 증가 부분 수열)
+	1-1.for(B[0] ~ B[n] 으로 끝나는 증가 부분 수열 bb)
 		{
-			합하여 최대 값 업데이트
+			1-1-1. aa + bb 길이 구함
 		}
 }
 
