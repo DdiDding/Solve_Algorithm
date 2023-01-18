73795d70586de1a84aa34217174129734d192c3d
yeo myoung Kim <kdjwand@naver.com>
2023-01-18 19:56:49 +0900

    병합 전 파일정리
    병합 전 파일정리

--- Greedy/2839.cpp
+++ Greedy/2839.cpp
@@ -1,41 +1,52 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-	int kg = 0;
-	int cnt = 0;
-	scanf("%d", &kg);
-
-	//우선 5로 나누어 떨어지는지 확인
-	if (kg % 5 == 0)
-	{
-		printf("%d", kg / 5);
-		return 0;
-	}
-
-	//KG가 15 이하가 될 때까지 5씩 줄인다.
-	while (15 < kg)
-	{
-		kg -= 5;
-		++cnt;
-	}
-
-	//그리고 최대 단위인 3미만이면 -1을 출력
-	while (3 <= kg)
-	{
-		//일단 현재 KG이 최소 공배수인 15미만이기 때문에 3으로 나누어떨어지면 5로 나누어 떨어질수가 없다.
-		if (kg % 3 == 0)
-		{
-			cnt += kg / 3;
-			printf("%d", cnt);
-			return 0;
-		}
-
-		kg -= 5;
-		++cnt;
-	}
-
-	printf("%d", -1);
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0), cin.tie(0);
+//	int n = 0; cin >> n;
+//	int cnt = 0;
+//
+//	int three = 0;
+//	if (n % 5 != 0)
+//	{
+//		while (0 <= n)
+//		{
+//			n -= 3;
+//			three++;
+//
+//			if (n % 5 == 0) break;
+//		}
+//	}
+//	
+//	cnt = n / 5 + three;
+//	cout << (n < 0 ? -1 : cnt);
+//	return 0;
+//}
+///*
+//상근 - 설탕배달
+//N 만큼 배달
+//3, 5 Kg 봉지 있음
+//
+//최대한 적은 봉지로 배달 
+//
+//+6,
+//+9
+//+12
+//+18
+//+21
+//+24
+//+27
+//+30
+//+33
+//+36
+//+39
+//
+//
+//
+//21/15 =6
+//
+//57 / 15 = 45 ,, 27
+//
+//
+//*/
