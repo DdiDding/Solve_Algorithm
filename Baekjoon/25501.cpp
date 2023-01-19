f10ff076bb959bb73731225f6738887f09973890
yeo myoung Kim <kdjwand@naver.com>
2023-01-19 23:51:37 +0900

    25501 성공

--- Baekjoon/25501.cpp
+++ Baekjoon/25501.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int ret;
-int recursion(string s, int l, int r) {
+int recursion(string & s, int l, int r) {
 	++ret;
 	if (l >= r) return 1;
 	else if (s[l] != s[r]) return 0;
 	else return recursion(s, l + 1, r - 1);
 }
 
-int isPalindrome(string s) {
+int isPalindrome(string & s) {
 	return recursion(s, 0, s.size() - 1);
 }
 
