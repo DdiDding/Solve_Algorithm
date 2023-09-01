7e9746e75400a1473cd70282bcf324830317dc84
여명 김 <kdjwand@naver.com>
2023-09-02 02:05:48 +0900

    158A 성공

---
+++ Codeforce/158A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* k위의 이상 양수(1이상) 점수를 획득한 참가자는 다음 라운드에 진출한다.
+ * 대회의 총 n명의 참가자가 주어진다, 그리고 당신은 그들의 점수를 알 수 있다.
+ * 얼만큼의 참가자가 다음 라운드에 진출하는지 계산하세요
+
+ * Input : 첫 번째 줄의 input은 두개의 정수 n과 k가 빈칸 하나 차이로 들어있다
+ * 두 번째 줄은 n개의 정수가 띄어서 들어온다
+ */
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	int n, k; cin >> n >> k;
+	
+	std::vector<int> scores;
+	scores.resize(n);
+	int cnt = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> scores[i];
+		if (scores[k] <= scores[i]) ++cnt;
+	}
+
+	cout << cnt;
+	return 0;
+}
