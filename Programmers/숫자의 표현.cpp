0d205601d492879e79b72416cdd76a2d6107dbd7
여명 김 <kdjwand@naver.com>
2023-09-29 17:38:52 +0900

    숫자의 표현 재풀이 성공

--- Programmers/숫자의 표현.cpp
+++ Programmers/숫자의 표현.cpp
@@ -3,13 +3,25 @@
 
 using namespace std;
 
-int dp[10'005];
-
 int solution(int n) {
 	int answer = 0;
 
-	for (int i = n ; )
+	int ret = 0;
+	int last = n;
+	for (int s = n; 0 < s; --s)
+	{
+		//시작 num 추가
+		ret += s;
 
+		//크거나 같으면
+		if (n <= ret)
+		{
+			//같으면 카운팅
+			if (n == ret) answer++;
+			//마지막 숫자 제거 후, 마지막 숫자 -1
+			ret -= last--;
+		}
+	}
 
 	return answer;
 }
